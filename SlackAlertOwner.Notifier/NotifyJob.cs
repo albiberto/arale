66b881a5a092d69dcc31c539b6bf725e646e1182
Alberto Viezzi <aviezzi@outlook.com>
2019-12-04 22:46:26 +0100

    Add Quartz.

---
+++ SlackAlertOwner.Notifier/NotifyJob.cs
@@ -0,0 +1,21 @@
+﻿namespace SlackAlertOwner.Notifier
+{
+    using Microsoft.Extensions.Logging;
+    using Quartz;
+    using System.Threading.Tasks;
+
+    public class NotifyJob : IJob
+    {
+        private readonly ILogger<NotifyJob> _logger;
+        public NotifyJob(ILogger<NotifyJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Start NotifyJob");
+            return Task.CompletedTask;
+        }
+    }
+}
