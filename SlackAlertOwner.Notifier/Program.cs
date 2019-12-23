2645d983fb5a68e8dec1dd293aac7e049652006b
Alberto Viezzi <aviezzi@outlook.com>
2019-12-23 23:46:29 +0100

    Refactoring.

--- SlackAlertOwner.Notifier/Program.cs
+++ SlackAlertOwner.Notifier/Program.cs
@@ -3,19 +3,11 @@ namespace SlackAlertOwner.Notifier
     using Abstract;
     using Clients;
     using Converters;
-    using Jobs;
-    using Microsoft.Extensions.Configuration;
+    using IoC;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Options;
-    using Model;
     using NodaTime;
-    using Quartz;
-    using Quartz.Impl;
-    using Quartz.Spi;
     using Services;
-    using System;
-    using System.IO;
 
     public class Program
     {
@@ -28,28 +20,9 @@ namespace SlackAlertOwner.Notifier
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    var configBuilder = new ConfigurationBuilder()
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("appsettings.json", true);
-                    var config = configBuilder.Build();
-
-                    services.Configure<MyOptions>(config.GetSection("Options"));
-
-                    services.AddSingleton<IJobFactory, SingletonJobFactory>();
-                    services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
-                    // services.AddSingleton<NotifyJob>();
-                    services.AddSingleton<CalendarJob>();
-
-                    // services.AddSingleton(provider => new JobSchedule(typeof(NotifyJob), provider.GetService<IOptions<MyOptions>>().Value.CronExpression));
-                    services.AddSingleton(provider => new JobSchedule(typeof(CalendarJob), provider.GetService<IOptions<MyOptions>>().Value.CronExpression));
-                    
-                    services.AddHostedService<QuartzHostedService>();
-
-                    services.AddHttpClient("cazzeggingZoneClient",
-                        (provider, client) =>
-                        {
-                            client.BaseAddress = new Uri(provider.GetService<IOptions<MyOptions>>().Value.BaseUrl);
-                        });
+                    services.AddOptionsPattern();
+                    services.AddQuartz();
+                    services.AddHttpClients();
 
                     services.AddSingleton<ISlackHttpClient, SlackHttpClient>();
                     services.AddSingleton<IAlertOwnerSpreadSheetService, AlertOwnerSpreadSpreadSheetService>();
@@ -58,6 +31,8 @@ namespace SlackAlertOwner.Notifier
                     services.AddSingleton<IShiftsCalendarService, ShiftsCalendarService>();
                     services.AddSingleton<ITimeService, TimeService>();
                     services.AddSingleton<ISpreadSheetService, SpreadSheetService>();
+
+                    services.AddHostedService<QuartzHostedService>();
                 });
     }
 }
