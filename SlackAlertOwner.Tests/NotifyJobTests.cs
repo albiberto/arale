2b98fbd5b51e38664fda5bc41815566bcdba5148
Alberto Viezzi <aviezzi@outlook.com>
2020-01-04 22:28:20 +0100

    Add tests.

--- SlackAlertOwner.Tests/NotifyJobTests.cs
+++ SlackAlertOwner.Tests/NotifyJobTests.cs
@@ -18,8 +18,16 @@
             _repository = new MockRepository(MockBehavior.Strict) {DefaultValue = DefaultValue.Mock};
 
             _slackHttpClient = _repository.Create<ISlackHttpClient>();
+
             _alertOwnerService = _repository.Create<IAlertOwnerService>();
-            _logger = _repository.Create<ILogService>();
+            _alertOwnerService
+                .Setup(s => s.GetTeamMates())
+                .ReturnsAsync(_teamMates);
+            
+            var logger = _repository.Create<ILogService>();
+            logger.Setup(s => s.Log(It.IsAny<string>()));
+
+            _logger = logger.Object;
         }
 
         [TearDown]
@@ -32,7 +40,7 @@
 
         Mock<ISlackHttpClient> _slackHttpClient;
         Mock<IAlertOwnerService> _alertOwnerService;
-        Mock<ILogService> _logger;
+        ILogService _logger;
 
         readonly IEnumerable<TeamMate> _teamMates = new List<TeamMate>
         {
@@ -41,34 +49,65 @@
             new TeamMate("3", "Thor", "AZ")
         };
 
+        readonly TeamMate _ironMan = new TeamMate("1", "IronMan", "US");
+        readonly TeamMate _hulk = new TeamMate("2", "Hulk", "LA");
+
         [Test]
-        public async Task Should_run_NotifyJob()
+        public async Task Should_notify_in_the_middle_of_the_week()
         {
-            _alertOwnerService
-                .Setup(s => s.GetTeamMates())
-                .ReturnsAsync(_teamMates);
-
-            var ironMan = new TeamMate("1", "IronMan", "US");
-            var hulk = new TeamMate("2", "Hulk", "LA");
-
-            var today = new Shift(ironMan, new LocalDate(2019, 12, 1));
-            var tomorrow = new Shift(hulk, new LocalDate(2019, 12, 2));
-            var shifts = (today, tomorrow);
+            var today = new Shift(_ironMan, new LocalDate(2019, 12, 3));
+            var tomorrow = new Shift(_hulk, new LocalDate(2019, 12, 4));
 
             _alertOwnerService
                 .Setup(s =>
                     s.GetShift(It.Is<IEnumerable<TeamMate>>(tm => Equals(tm, _teamMates))))
-                .ReturnsAsync(shifts);
+                .ReturnsAsync((today, tomorrow));
 
             _slackHttpClient
-                .Setup(s => s.Notify(It.IsAny<List<string>>()))
+                .Setup(s => s.Notify(It.IsAny<string>()))
                 .Returns(Task.CompletedTask);
 
-            _logger.Setup(s => s.Log(It.IsAny<string>()));
-
-            var sut = new NotifyJob(_slackHttpClient.Object, _alertOwnerService.Object, _logger.Object);
-
+            var sut = new NotifyJob(_slackHttpClient.Object, _alertOwnerService.Object, _logger);
             await sut.Execute(null);
+
+            _slackHttpClient
+                .Verify(v => v.Notify(It.IsAny<string>()), Times.Exactly(2));
+        }
+        
+        [Test]
+        public async Task Should_not_notify_in_the_weekend()
+        {
+            _alertOwnerService
+                .Setup(s =>
+                    s.GetShift(It.Is<IEnumerable<TeamMate>>(tm => Equals(tm, _teamMates))))
+                .ReturnsAsync((null, null));
+
+            var sut = new NotifyJob(_slackHttpClient.Object, _alertOwnerService.Object, _logger);
+            await sut.Execute(null);
+
+            _slackHttpClient
+                .Verify(v => v.Notify(It.IsAny<string>()), Times.Never);
+        }
+        
+        [Test]
+        public async Task Should_notify_in_only_one_at_friday()
+        {
+            var today = new Shift(_ironMan, new LocalDate(2019, 12, 3));
+
+            _alertOwnerService
+                .Setup(s =>
+                    s.GetShift(It.Is<IEnumerable<TeamMate>>(tm => Equals(tm, _teamMates))))
+                .ReturnsAsync((today, null));
+            
+            _slackHttpClient
+                .Setup(s => s.Notify(It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+
+            var sut = new NotifyJob(_slackHttpClient.Object, _alertOwnerService.Object, _logger);
+            await sut.Execute(null);
+
+            _slackHttpClient
+                .Verify(v => v.Notify(It.IsAny<string>()), Times.Once);
         }
     }
 }
