dcd7ee371ce6a9c73ac7e668aeff5bec3885a90a
Alberto Viezzi <aviezzi@outlook.com>
2020-01-12 21:19:32 +0100

    Start service in deploy.ps1

--- SlackAlertOwner.Notifier/deploy.ps1
+++ SlackAlertOwner.Notifier/deploy.ps1
@@ -1,3 +1,18 @@
-﻿dotnet publish --configuration Release --self-contained -r win10-x64 -o ..\dist
+﻿# Generate .exe
+dotnet publish --configuration Release --self-contained -r win10-x64 -o C:\dist\AraleService
 
-New-Service -Name "AraleService" -BinaryPathName "C:\Users\aviez\Projects\SlackAlertOwnerService\dist\SlackAlertOwner.Notifier.exe" -Description "Slack Alert Owner Notifier Service" -DisplayName "Arale" -StartupType Automatic
+# Copy certificate in dist folder
+Copy-Item "certificate.p12" -Destination "C:\dist\AraleService"
+
+# Deploy WindowsService.
+$params = @{
+    Name = "AraleService"
+    BinaryPathName = "C:\dist\AraleService\SlackAlertOwner.Notifier.exe"
+    DisplayName = "Arale"
+    StartupType = "Manual"
+    Description = "Slack Alert Owner Notifier Service"
+}
+New-Service @params
+
+# Start Service
+Start-Service -Name "AraleService"
